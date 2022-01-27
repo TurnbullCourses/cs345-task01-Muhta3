3b3f164c6ebbae3045ebd8e135da5be850e57d07
Laci Young <lyoung4@ithaca.edu>
2022-01-27 10:21:57 -0500

    must commit to pull...

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -27,7 +27,7 @@ class BankAccountTest {
         assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
         assertFalse( BankAccount.isEmailValid(""));         // empty string
 
-        assertTrue(BankAccount.isEmailValid("1234atgmaildotcom"));
+        //Write tests here that fail but can pass when I edit your isEmailValid function 
 
         
     }
